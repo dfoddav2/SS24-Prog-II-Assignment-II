c0e06e910b0c6ec76917974cbf2b3c73b0c10c6d
David Fodor <david.fodorhivatalos@gmail.com>
2024-06-21 12:12:07 +0200

    Squashed 'backend/' changes from 3ed6bcf..a791e11
    a791e11 Fixed Multiplayer Wortle solution, -1 placeholder user, mongo-uri env
    
    git-subtree-dir: backend
    git-subtree-split: a791e1158845008fde4e314d6eeb895c3f0e68d8

--- start.py
+++ start.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     client = app.test_client()
     response = client.get("/")
     print(response)
-    socketio.run(app, debug=True, port=7890)  # Use socketio.run instead of app.run
+    socketio.run(app, port=7890)  # Use socketio.run instead of app.run
     
